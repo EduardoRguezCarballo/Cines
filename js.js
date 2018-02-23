79460ef1e5a04e9b8fbd523d3db34e443cba8b8a
Eduardo <eduguia2009@gmail.com>
2018-02-23 18:33:58 +0000

    actualizando carteles

--- js.js
+++ js.js
@@ -69,114 +69,114 @@ $(document).ready(function () {
 function montaJson() {
 	dato = [
 		{
-			'titulo': 'Dando La Nota',
-			'imagen': 'dando-la-nota-3.jpg',
+			'titulo': 'The Shape Of Water',
+			'imagen': 'the-shape-of-water.jpg',
 			'trailer': 'img/video1.PNG',
-			'director': 'Trish Sie.',
-			'Reparto': 'Anna Kendrick, Rebel Wilson, Hailee Steinfeld, Brittany Snow, Anna Camp.',
-			'Genero': 'Comedia, musical.',
-			'duracion': '93min.',
-			'estreno': '29/12/17.',
-			'sinopsis': 'Tras ganar el Campeonato del Mundo, las Bellas se separan para seguir con sus vidas hasta que se dan cuenta de que la música a capella no tiene muchas salidas laborales. Pero cuando surge la oportunidad de organizar una gira por el extranjero, el destino reunirá por última vez a estas peculiares y únicas estrellas para componer y plantearse una serie de decisiones que no podrán tomar a la ligera.'
+			'director': 'Guillermo Del Toro',
+			'Reparto': 'Sally Hawkins, Octavia Spencer, Michael Stuhlbarg, Richard Jenkins, Michael Shannon',
+			'Genero': 'drama, fantasy',
+			'duracion': '123 min',
+			'estreno': '16/02/18',
+			'sinopsis': 'En un inquietante laboratorio de alta seguridad, durante la Guerra Fría, se produce una conexión insólita entre dos mundos, aparentemente alejados. La vida de la solitaria Elisa, trabajadora del laboratorio, cambia por completo cuando descubre un experimento clasificado como secreto.'
 		},
 		{
-			'titulo': 'Insidious La última llave',
-			'imagen': 'insidious-la-ultima-llave.jpg',
+			'titulo': 'Black Panther',
+			'imagen': 'black-panther.jpg',
 			'trailer': 'img/video2.png',
-			'director': 'Adam Robitel.',
-			'Reparto': 'Spencer Locke, Javier Botet, Josh Stewart, Lin Shaye, Kirk Acevedo.',
-			'Genero': 'Terror, thriller.',
-			'duracion': '103 min.',
-			'estreno': '05/01/18.',
-			'sinopsis': 'En esta nueva película de terror original, en la que veremos de nuevo a Lin Shaye como la Doctora Elise Rainier, la brillante parapsicóloga se enfrenta a su más personal y aterradora caza hasta la fecha, que sucede en su antigua casa familiar.'
+			'director': 'Ryan Coogler',
+			'Reparto': 'Chadwick Boseman, Michael B. Jordan, Lupita Nyong\'O, Danai Gurira, Martin Freeman',
+			'Genero': 'Acción, aventuras, ciencia-ficción',
+			'duracion': '134 min',
+			'estreno': '16/02/18',
+			'sinopsis': '\'Black Panther\' cuenta la historia de T\'Challa quien, después de los acontecimientos de \'Capitán América: Civil War\', vuelve a casa, a la nación de Wakanda, aislada y muy avanzada tecnológicamente, para ser proclamado Rey. Pero la reaparición de un viejo enemigo pone a prueba el temple de T\'Challa como Rey y Black Panther ya que se ve arrastrado a un conflicto que pone en peligro todo el destino de Wakanda y del mundo.'
 		},
 		{
-			'titulo': 'Jumanji',
-			'imagen': 'jumanji.jpg',
+			'titulo': 'Yo, Tonya',
+			'imagen': 'yo-tonya.jpg',
 			'trailer': 'img/video3.PNG',
-			'director': 'Jake Kasdan.',
-			'Reparto': 'Dwayne Johnson, Karen Gillan, Kevin Hart, Jack Black, Nick Jonas.',
-			'Genero': 'Acción, aventuras, comedia, fantasía',
+			'director': 'Craig Gillespie',
+			'Reparto': 'Margot Robbie, Sebastian Stan, Allison Janney, Julianne Nicholson, Bobby Cannavale',
+			'Genero': 'Biografía, comedia, drama',
 			'duracion': '119 min.',
-			'estreno': '22/12/17.',
-			'sinopsis': 'Cuatro compañeros de instituto descubren un vieja videoconsola con un juego del que nunca han oído hablar, Jumanji, y se sienten inmediatamente atraídos por la ambientación selvática del juego, para entonces verse transformados en los avatares que han elegido: el jugón Spencer se convierte en un musculoso aventurero (Dwayne Johnson), la estrella de fútbol americano Fridge pierde (en palabras propias) "los 60 cm superiores de su cuerpo" y se convierte en un genio (Kevin Hart), la chica popular Bethany se convierte en un profesor de mediana edad (Jack Black) y la tímida y apocada Martha se convierte en una aguerrida luchadora (Karen Gillan). Por si eso fuera poco, descubren que no solo están jugando a Jumanji, sino que también tendrán que sobrevivir en él. Para superar el juego y regresar al mundo real, deberán embarcarse en la aventura más peligrosa de sus vidas, descubrir qué fue lo que dejó Alan Parrish 20 años antes y cambiar la forma que tienen de verse a sí mismos, o quedarán atrapados en el juego para siempre...'
+			'estreno': '23/02/18',
+			'sinopsis': 'Tonya Harding (Margot Robbie) dominó el hielo con un estilo de patinaje totalmente único. También dominó los titulares por algo completamente diferente. "YO, TONYA" es, por momentos, un absurdo, trágico e hilarante retrato de la mujer en el centro del mayor escándalo en la historia del Deporte.'
 		},
 		{
-			'titulo': "Molly's Game",
-			'imagen': 'mollys-game.jpg',
+			'titulo': "Deber cumplido",
+			'imagen': 'deber-cumplido.jpg',
 			'trailer': 'img/video4.PNG',
-			'director': 'Aaron Sorkin.',
-			'Reparto': 'Jessica Chastain, Idris Elba, Michael Cera, Kevin Costner, Jeremy Strong.',
-			'Genero': 'BIografía, drama.',
-			'duracion': '140 min.',
-			'estreno': '05/01/18.',
-			'sinopsis': "Molly's Game' es la historia real de una mujer muy especial, Molly Bloom, quien durante una década se convirtió en la princesa de las partidas de póker clandestinas más cotizadas de Estados Unidos. Por sus partidas pasaron las más grandes estrellas de Hollywood y numerosas personalidades del mundo del deporte, los negocios e incluso de la mafia. Pero su castillo de naipes se derrumbó cuando el FBI la convirtió en su objetivo."
+			'director': 'Jason Hall',
+			'Reparto': 'Miles Teller, Beulah Koale, Joe Cole, Scott Haze, Haley Bennett, Amy Schumer',
+			'Genero': 'Bélica, biografía, drama',
+			'duracion': '108 min',
+			'estreno': '16/02/18',
+			'sinopsis': "La película cuenta la historia de Adam Schumann (Miles Teller), un militar que regresa a casa tras cumplir con su servicio en el ejército norteamericano en Irak. Con su llegada, las cosas no serán fáciles ni para él ni para su familia ya que no logra encontrar la paz debido al trastorno de estrés post traumático que padece. Esto hará que integrarse a la sociedad se convierta en un camino muy difícil de alcanzar y, al mismo tiempo, estará obligado a afrontar los horribles recuerdos que le dejó la guerra."
 		},
 		{
-			'titulo': "El gran showman",
-			'imagen': 'the-greatest-showman.jpg',
+			'titulo': "El cuaderno de Sara",
+			'imagen': 'el-cuaderno-de-sara.jpg',
 			'trailer': 'img/video5.PNG',
-			'director': 'Michael Gracey.',
-			'Reparto': 'Hugh Jackman, Zac Efron, Michelle Williams, Zendaya, Rebecca Ferguson.',
-			'Genero': 'Biografía, drama, musical.',
-			'duracion': '105 min.',
-			'estreno': '29/12/17.',
-			'sinopsis': "El Gran Showman es un audaz y original musical que celebra el nacimiento de la industria del espectáculo y la sensación de asombro que nos domina cuando los sueños se hacen realidad. Inspirado en la ambición y la imaginación de P.T. Barnum, 'El Gran Showman' narra la historia de un visionario que salió de la nada para crear un fascinante espectáculo que se convirtió en una sensación mundial."
+			'director': 'Norberto López Amado',
+			'Reparto': 'Belén Rueda, Manolo Cardona, Enrico Lo Verso, Marian Álvarez, Florin Opritescu',
+			'Genero': 'Drama, thriller',
+			'duracion': '115 min.',
+			'estreno': '02/02/18',
+			'sinopsis': "Desde hace años Laura busca a su hermana Sara, desaparecida en medio de la selva del Congo. Ni la ONG para la que trabaja, ni la embajada tienen noticias de su paradero hasta que aparece una foto de Sara en un poblado minero del este del Congo. Laura decide viajar a Kampala para desde allí iniciar un peligroso viaje al corazón del África, un territorio dominado los señores de la guerra. Una aventura que la llevará hasta la más sucia, violenta y oculta trastienda de los poderes occidentales."
 		},
 		{
-			'titulo': "Mazinger Z Infinity",
-			'imagen': 'mazinger-z-infinity.jpg',
+			'titulo': "C'est la vie!",
+			'imagen': 'cest-la-vie.jpg',
 			'trailer': 'img/video6.PNG',
-			'director': 'Junji Shimizu.',
-			'Reparto': '(ANIMACIÓN).',
-			'Genero': 'Animación.',
-			'duracion': '90 min.',
-			'estreno': '19/01/18.',
-			'sinopsis': "La humanidad se encuentra una vez más en peligro de caer en las garras del Imperio Subterráneo, que en el pasado fue liderado por el científico malvado Dr. Infierno. Entonces, Koji Kabuto pilotó al súper robot Mazinger Z y, con la ayuda de sus amigos del Instituto de Investigaciones Fotónicas, frustró las malvadas ambiciones del Dr. Infierno y devolvió la paz al mundo. Han transcurrido 10 años desde entonces... Ahora que ya no es piloto, Koji Kabuto ha seguido el camino de su padre y su abuelo iniciando su carrera como científico. Y ahora encuentra una gigantesca estructura enterrada en las profundidades del Monte Fuji, junto con una misteriosa indicación de la vida... Nuevos encuentros, nuevas amenazas y un nuevo destino aguardan a la humanidad. El antiguo héroe Koji Kabuto debe tomar una decisión sobre el futuro: ser un dios o un demonio..."
+			'director': 'Eric Toledano, Olivier Nakache',
+			'Reparto': 'Suzanne Clément, Gilles Lellouche, Jean-Paul Rouve, Judith Chemla, Jean-Pierre Bacri',
+			'Genero': 'Comedia',
+			'duracion': '117 min.',
+			'estreno': '09/02/18',
+			'sinopsis': "Max ha organizado cientos de bodas durante años y ahora prepara una boda de lujo en un château francés del siglo XVIII. Ha coordinado todo para que la celebración sea un éxito: camareros, orquesta, menú, disc jockey, decoración floral... Pero el resultado pende de un hilo y cada momento de felicidad y emoción puede convertirse en desastre o caos. ¿A quién confiarías la fiesta más importante de tu vida?"
 		},
 		{
-			'titulo': "Perfectos desconocidos",
-			'imagen': 'perfectos-desconocidos.jpg',
+			'titulo': "Cavernícola",
+			'imagen': 'cavernicola.jpg',
 			'trailer': 'img/video7.PNG',
-			'director': 'Álex De La Iglesia.',
-			'Reparto': 'Belén Rueda, Eduardo Noriega, Dafne Fernández, Juana Acosta, Ernesto Alterio.',
-			'Genero': 'Comedia, drama.',
-			'duracion': '96 min.',
-			'estreno': '01/12/17.',
-			'sinopsis': "Una extraña inquietud parece haberse apoderado de la ciudad. El tráfico se colapsa, las urgencias de los hospitales están a rebosar, los perros aúllan intuyendo el peligro que se aproxima: es el eclipse de luna. Su poderoso influjo afecta también a los protagonistas de esta historia. Los anfitriones discuten alterados. Sus amigos están a punto de llegar y la cena no está preparada todavía. Alfonso y Eva: los dueños de la casa, profesionales bien situados, preocupados por su hija adolescente y por lo monótona que se ha vuelto su vida. Eduardo y Blanca: ella se quiere casar, él no; ella quiere tener un hijo, él ni se lo plantea. Antonio y Ana: con dos niños, a punto del divorcio, algo que descubrirán esa misma noche. Pepe: profesor en paro, su novia está enferma y les dice que por eso no ha podido venir ¿o es tan sólo una excusa? Una reunión de amigos como tantas otras, hasta que, de pronto, surge la idea. ¿Por qué no hacer algo distinto? Vamos a jugar a un juego. ¿Qué pasaría si dejásemos nuestros móviles encima de la mesa, al alcance de todos? Llamadas, SMS, Whatsapps, notificaciones de Instagram o Facebook, nuestra vida entera compartida al instante por todo el mundo."
+			'director': 'Nick Park',
+			'Reparto': '(ANIMACIÓN)',
+			'Genero': 'Animación, aventuras, comedia',
+			'duracion': '89 min',
+			'estreno': '02/02/18',
+			'sinopsis': "Dug, humano y Hognob, jabalí, son los protagonistas de esta nueva aventura de Aardman en la que dos civilizaciones, la de la Edad de Piedra y la de Bronce, compiten por hacerse hueco en la historia (mientras que por casualidad inventan el fútbol). Dug y Hognob tendrán que ayudar a su tribu, sorteando con gracia y astucia múltiples peligros y derrotando al malvado Lord Nooth para no perder su hogar."
+		},
+		{
+			'titulo': " Cincuenta sombras liberadas",
+			'imagen': 'cincuenta-sombras-liberadas.jpg',
+			'trailer': 'img/video8.PNG',
+			'director': 'James Foley',
+			'Reparto': 'Dakota Johnson, Jamie Dornan, Eric Johnson, Eloise Mumford, Rita Ora',
+			'Genero': 'Drama, romance, thriller',
+			'duracion': '101 min',
+			'estreno': '09/02/18',
+			'sinopsis': "Tercera entrega de la romántica, sensual, erótica y totalmente adictiva historia de la apasionada relación entre una estudiante universitaria y un joven multimillonario. En esta ocasión Anastasia y Christian se casan, pero Jack Hyde (Eric Johnson) continúa amenazando su relación."
 		},
 		{
 			'titulo': "Coco",
 			'imagen': 'coco.jpg',
-			'trailer': 'img/video8.PNG',
-			'director': 'Lee Unkrich, Adrian Molina.',
-			'Reparto': 'Benjamin Bratt, Anthony Gonzalez, Gael García Bernal, Renée Victor, Ana Ofelia Murguía.',
-			'Genero': 'Animación, Aventuras, Comedia.',
-			'duracion': '109 min.',
-			'estreno': '01/12/17.',
-			'sinopsis': "A pesar de las desconcertantes tradiciones de su familia entre las que se encuentra la vieja prohibición de estar en contacto con la música, Miguel (voz de Anthony Gonzalez) sueña convertirse en un consumado éxito como su ídolo, Ernesto de la Cruz (voz de Benjamín Bratt). Desesperado por demostrar su talento, Miguel se encuentra en la impresionante y colorida Tierra de los Muertos siguiendo una misteriosa cadena de eventos. A lo largo del camino, conocerá al encantador y tramposo Héctor (voz de Gael García Bernal), y juntos emprenderán un viaje extraordinario para desvelar la verdad que hay detrás de la historia familiar de Miguel."
-		},
-		{
-			'titulo': "Star Wars",
-			'imagen': 'star-wars.jpg',
-			'trailer': 'img/video10.PNG',
-			'director': 'Rian Johnson.',
-			'Reparto': 'Mark Hamill, Carrie Fisher, Adam Driver, Daisy Ridley, John Boyega.',
-			'Genero': 'Acción, aventuras, ciencia-ficción, fantasía',
-			'duracion': '152 min.',
-			'estreno': '15/12/17.',
-			'sinopsis': "En 'Star Wars: Los últimos Jedi', la saga de Skywalker continúa mientras los héroes de 'El despertar de la fuerza' se unen a las leyendas galácticas en una aventura épica que desbloqueará viejos misterios de la Fuerza y sorprendentes revelaciones del pasado."
-		},
-		{
-			'titulo': "El extranjero",
-			'imagen': 'el-extranjero.jpg',
 			'trailer': 'img/video9.png',
-			'director': 'Martin Campbell.',
-			'Reparto': 'Jackie Chan, Pierce Brosnan, Charlie Murphy, Katie Leung, Simon Kunz.',
-			'Genero': 'Acción, drama, thriller.',
-			'duracion': '114 min.',
+			'director': 'Lee Unkrich, Adrian Molina ',
+			'Reparto': 'Benjamin Bratt, Anthony Gonzalez, Gael García Bernal, Renée Victor, Ana Ofelia Murguía',
+			'Genero': 'Animación, Aventuras, Comedia',
+			'duracion': '109 min',
 			'estreno': '12/01/18.',
-			'sinopsis': 'El Extranjero" cuenta la historia de Quan (Jackie Chan), un humilde dueño de un restaurante londinense cuyo pasado, ya enterrado, emerge cargado de venganza al perder a su hija, la única familia que le queda, en un acto terrorista con motivaciones políticas. Mientras busca a los responsables, recurre a la ayuda del Viceministro irlandés, Liam Hennessy (Pierce Brosnan), un funcionario del gobierno a quien afecta su propio pasado turbio de vinculación con el grupo terrorista IRA. Así comienza un juego del gato y el ratón con tinte político entre Quan y Hennessy, quienes deben confrontar su pasado mientras intentan identificar a los escurridizos asesinos.'
+			'sinopsis': 'A pesar de las desconcertantes tradiciones de su familia entre las que se encuentra la vieja prohibición de estar en contacto con la música, Miguel (voz de Anthony Gonzalez) sueña convertirse en un consumado éxito como su ídolo, Ernesto de la Cruz (voz de Benjamín Bratt). Desesperado por demostrar su talento, Miguel se encuentra en la impresionante y colorida Tierra de los Muertos siguiendo una misteriosa cadena de eventos. A lo largo del camino, conocerá al encantador y tramposo Héctor (voz de Gael García Bernal), y juntos emprenderán un viaje extraordinario para desvelar la verdad que hay detrás de la historia familiar de Miguel.'
+		},
+		{
+			'titulo': "El corredor del laberinto: La cura mortal",
+			'imagen': 'el-corredor-del-laberinto-la-cura-mortal.jpg',
+			'trailer': 'img/video10.png',
+			'director': 'Wes Ball',
+			'Reparto': 'Kaya Scodelario, Dylan O\'Brien, Nathalie Emmanuel, Katherine McNamara, Walton Goggins',
+			'Genero': 'Acción, ciencia-ficción, thriller',
+			'duracion': '142 min',
+			'estreno': '26/01/18',
+			'sinopsis': 'En este final épico de la saga Maze Runner, Thomas lidera su grupo de Gladers escapados de la última y difícil misión. Para salvar a sus amigos, deben entrar en la legendaria Last City, un laberinto controlado por WCKD que puede llegar a ser el laberinto más mortífero de todos. Cualquier persona que lo consiga pasar con vida obtendrá respuestas a las preguntas que los Gladers se han estado haciendo desde que llegaron por primera vez al laberinto.'
 		}
 	];
 }
@@ -274,16 +274,16 @@ function dibujar() {
 
 function compruebaInfo(){
 
-    var pelicula1 = ["Dando La Nota",0,0];
-    var pelicula2 = ["Insidious La última llave",0,0];
-    var pelicula3 = ["Jumanji",0,0];
-    var pelicula4 = ["Molly's Game",0,0];
-    var pelicula5 = ["El gran showman",0,0];
-    var pelicula6 = ["Mazinger Z Infinity",0,0];
-    var pelicula7 = ["Perfectos desconocidos",0,0];
-    var pelicula8 = ["Coco",0,0];
-    var pelicula9 = ["Star Wars",0,0];
-    var pelicula10 = ["El extranjero",0,0];
+    var pelicula1 = ["The Shape Of Water",0,0];
+    var pelicula2 = ["Black Panther",0,0];
+    var pelicula3 = ["Yo, Tonya",0,0];
+    var pelicula4 = ["Deber cumplido",0,0];
+    var pelicula5 = ["El cuaderno de Sara",0,0];
+    var pelicula6 = ["C'est la vie",0,0];
+    var pelicula7 = ["Cavernícola",0,0];
+    var pelicula8 = ["Cincuenta sombras liberadas",0,0];
+    var pelicula9 = ["Coco",0,0];
+    var pelicula10 = ["El corredor del laberinto: La cura mortal",0,0];
     
     var datos_guardados = localStorage.getItem('Votacionpelicula');
     datos_guardados = datos_guardados.split('/');
