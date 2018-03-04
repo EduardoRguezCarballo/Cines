c827b802f3c63d9a543a1cfda6c65098c4321f49
Eduardo <eduguia2009@gmail.com>
2018-03-04 13:59:52 +0000

    montaje del formulario

--- compra/javaScript.js
+++ compra/javaScript.js
@@ -2,26 +2,62 @@ var myJSON;
 var pelicula;
 
 $(document).ready(function () {
-	cargar();	
+	
+	if (localStorage.getItem('titulo') == null) {
+		window.location.href = "../index.html";
+	}
+	
+	$('.datos__usuario').hide();
+	
+	var inputs = $('.formulario__input');
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].addEventListener('keyup', function(){
+			if(this.value.length >= 1){
+				this.nextElementSibling.classList.add('fijar');
+			}else{
+				this.nextElementSibling.classList.remove('fijar');
+			}
+		});
+	}
+	
+	cargar();
+	
+	$('#mostrarDatosPelicula').on('click',function(){
+		$('.datos__pelicula').hide();
+		$('.datos__usuario').show();
+	})
+	
+	$('#volverIndex').on('click',function(){
+		window.location.href = "../index.html";
+	})
+	
+	$('#VolverDatosPelicula').on('click',function(){
+		$('.datos__usuario').hide();
+		$('.datos__pelicula').show();
+	})
+	
+	$('#SeleccionarButacas').on('click', mostrarButacas);
 });
 
-function cargar(){
+function cargar() {
 	var titulo = localStorage.getItem('titulo');
-	var cartel = localStorage.getItem('cartel');
 	var horarios = localStorage.getItem('horas').split('/');
-	
-	$('#cartel')[0].src="../img"+cartel;
+
 	$('#titulo')[0].textContent = titulo;
-	
-	for(var i = 0; i<horarios.length; i++){
+
+	for (var i = 0; i < horarios.length; i++) {
 		var opcion = `<option value="${horarios[i]}">${horarios[i]}</option>`;
 		$('#Hora').append(opcion);
 	}
 	var fechaActual;
 	var f = new Date();
-	fechaActual = f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
-	$('#fecha')[0].textContent = fechaActual;	
-	
+	fechaActual = f.getDate() + "/" + (f.getMonth() + 1) + "/" + f.getFullYear();
+	$('#fecha')[0].textContent = fechaActual;
+
+}
+
+function mostrarButacas(){
+	alert()
 }
 
 function mensaje(cabecera, texto) {
